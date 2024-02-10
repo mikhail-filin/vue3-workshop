75463cec5b174f7751539b24cf46f9636f38076e
Mikhail Filin <mikhailf.codes@gmail.com>
2024-02-10 12:06:16 +0400

    feat: slots

--- atla-forum/src/App.vue
+++ atla-forum/src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import BenderStatistics from "./components/BenderStatistics.vue";
 import CharacterCard from "./components/CharacterCard.vue";
+import BaseLayout from "@/components/BaseLayout.vue";
 
 export default {
   components: {
     BenderStatistics,
     CharacterCard,
+    BaseLayout,
   },
   data: () => ({
     newCharacter: {
@@ -45,41 +47,53 @@ export default {
 </script>
 
 <template>
-  <BenderStatistics :characters="characterList" />
-  <h2>Characters</h2>
-  <p v-if="characterList.length === 0">There are no characters</p>
-  <ul v-else-if="characterList.length % 2 === 0">
-    <li
-      v-for="(character, index) in characterList"
-      :key="`even-character-${index}`"
-    >
-      <CharacterCard :character="character" @favorite="addFavoriteCharacter" />
-    </li>
-  </ul>
-  <p v-else>There are odd characters!</p>
-  <h2>Favorite Characters</h2>
-  <ul v-if="favoriteList.length > 0">
-    <li
-      v-for="(character, index) in favoriteList"
-      :key="`odd-character-${index}`"
-    >
-      {{ character }}
-    </li>
-  </ul>
-  <p v-else>No favorite characters yet!</p>
-  <h2>New Character</h2>
-  <pre>{{ newCharacter }}</pre>
-  <label for="character-name">Name</label>
-  <input
-    type="text"
-    v-model="newCharacter.name"
-    @keyup.enter="addNewCharacter"
-  />
-  <p>
-    <span
-      v-for="(character, index) in characterList"
-      :key="`comma-list-character-${index}`"
-      >{{ character.name }}{{ index === characterList.length - 1 ? "" : ", " }}
-    </span>
-  </p>
+  <BaseLayout>
+    <template #header> Some base header </template>
+    <template #aside>
+      <BenderStatistics :characters="characterList" />
+    </template>
+    <template #main>
+      <h2>Characters</h2>
+      <p v-if="characterList.length === 0">There are no characters</p>
+      <ul v-else-if="characterList.length % 2 === 0">
+        <li
+          v-for="(character, index) in characterList"
+          :key="`even-character-${index}`"
+        >
+          <CharacterCard
+            :character="character"
+            @favorite="addFavoriteCharacter"
+          />
+        </li>
+      </ul>
+      <p v-else>There are odd characters!</p>
+      <h2>Favorite Characters</h2>
+      <ul v-if="favoriteList.length > 0">
+        <li
+          v-for="(character, index) in favoriteList"
+          :key="`odd-character-${index}`"
+        >
+          {{ character }}
+        </li>
+      </ul>
+      <p v-else>No favorite characters yet!</p>
+      <h2>New Character</h2>
+      <pre>{{ newCharacter }}</pre>
+      <label for="character-name">Name</label>
+      <input
+        type="text"
+        v-model="newCharacter.name"
+        @keyup.enter="addNewCharacter"
+      />
+      <p>
+        <span
+          v-for="(character, index) in characterList"
+          :key="`comma-list-character-${index}`"
+          >{{ character.name
+          }}{{ index === characterList.length - 1 ? "" : ", " }}
+        </span>
+      </p>
+    </template>
+    <template #footer> Some base footer </template>
+  </BaseLayout>
 </template>
---
+++ atla-forum/src/components/BaseLayout.vue
@@ -0,0 +1,33 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <header class="header">
+    <slot name="header" />
+  </header>
+  <aside class="aside">
+    <slot name="aside" />
+  </aside>
+  <main class="main">
+    <slot name="main" />
+  </main>
+  <footer class="footer">
+    <slot name="footer" />
+  </footer>
+</template>
+
+<style scoped>
+.header {
+  border: 2px solid red;
+}
+.aside {
+  border: 2px solid blue;
+}
+.main {
+  border: 2px solid green;
+}
+.footer {
+  border: 2px solid black;
+}
+</style>
