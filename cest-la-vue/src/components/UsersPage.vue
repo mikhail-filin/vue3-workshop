c842bd0893e8c281062bd75225d35cc0fe1b6066
Mikhail Filin <mikhailf.codes@gmail.com>
2024-02-10 12:33:20 +0400

    feat: fetch with lifecycle hooks

---
+++ cest-la-vue/src/components/UsersPage.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data: () => ({
+    users: [],
+  }),
+  methods: {
+    async fetchUserData() {
+      this.users = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      ).then((response) => response.json());
+    },
+  },
+  created() {
+    this.fetchUserData();
+  },
+};
+</script>
+
+<template>
+  {{ users }}
+</template>
